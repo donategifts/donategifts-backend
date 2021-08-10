a363c0a07c11005840d4253c62c97adf4b3f8805
patric <ph@mycraftnote.de>
2021-08-10 12:31:56 +0200

    stuff

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,129 +1,199 @@
--- comment this line if you need persistent data during development
--- DROP DATABASE IF EXISTS donategifts;
-
-CREATE DATABASE IF NOT EXISTS donategifts;
-
-USE donategifts;
-
-CREATE TABLE IF NOT EXISTS address
-(
-    id        int PRIMARY KEY AUTO_INCREMENT,
-    address1  varchar(255) not null,
-    address2  varchar(255) null,
-    city      varchar(255) not null,
-    country   varchar(255) not null,
-    state     varchar(255) not null,
-    createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
-    updatedAt timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deletedAt datetime     null
+CREATE TYPE "ROLES" AS ENUM (
+  'GUEST',
+  'DONOR',
+  'AGENCY'
 );
 
-CREATE TABLE IF NOT EXISTS child
-(
-    id        int PRIMARY KEY AUTO_INCREMENT,
-    birthday  date         not null,
-    firstName varchar(255) not null,
-    lastName  varchar(255) null,
-    interest  varchar(255) not null,
-    bio       varchar(255) null,
-    addressId int          not null,
-    createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
-    updatedAt timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deletedAt datetime     null,
-    FOREIGN KEY (addressId) REFERENCES address (id)
+CREATE TYPE "AGENCY_ROLES" AS ENUM (
+  'OWNER',
+  'MEMBER'
 );
 
-CREATE TABLE IF NOT EXISTS user
-(
-    id                        int PRIMARY KEY AUTO_INCREMENT,
-    firstName                 varchar(255)                                    not null,
-    lastName                  varchar(255)                                    not null,
-    profileImage              varchar(255)                                    null,
-    email                     varchar(255) unique                             not null,
-    emailVerified             boolean   default false                         not null,
-    emailVerificationHash     varchar(255) unique                             null,
-    role                      enum ('donor', 'partner', 'admin')              not null,
-    loginMode                 enum ('facebook', 'google', 'default')          not null,
-    password                  varchar(255)                                    not null,
-    passwordResetToken        varchar(255) unique                             null,
-    passwordResetTokenExpires datetime                                        null,
-    createdAt                 timestamp DEFAULT CURRENT_TIMESTAMP,
-    updatedAt                 timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deletedAt                 datetime                                        null
+CREATE TYPE "WISHCARD_STATUS" AS ENUM (
+  'DRAFT',
+  'PUBLISHED',
+  'DONATED'
 );
 
-CREATE TABLE IF NOT EXISTS agency
-(
-    id         int PRIMARY KEY AUTO_INCREMENT,
-    name       varchar(255)            not null,
-    bio        varchar(255)            null,
-    isVerified boolean   DEFAULT false not null,
-    phone      varchar(255)            not null,
-    website    varchar(255)            null,
-    addressId  int unique              not null,
-    createdAt  timestamp DEFAULT CURRENT_TIMESTAMP,
-    updatedAt  timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deletedAt  datetime                null
+CREATE TYPE "DONATION_STATUS" AS ENUM (
+  'DONATED',
+  'SHIPPED',
+  'ARRIVED'
 );
 
-CREATE TABLE IF NOT EXISTS agency_members
-(
-    agencyId int                      not null,
-    userId   int                      not null,
-    role     enum ('admin', 'member') not null,
-    CONSTRAINT id PRIMARY KEY (agencyId, userId),
-    FOREIGN KEY (agencyId) REFERENCES agency (id),
-    FOREIGN KEY (userId) REFERENCES user (id)
+CREATE TYPE "GENDER" AS ENUM (
+  'MALE',
+  'FEMALE',
+  'OTHER'
 );
 
-CREATE TABLE IF NOT EXISTS wish_card
-(
-    id            int PRIMARY KEY AUTO_INCREMENT,
-    image         varchar(255)                         not null,
-    itemPrice     float                                not null,
-    itemUrl       varchar(255)                         not null,
-    childId       int                                  not null,
-    agencyId      int                                  not null,
-    createdBy     int                                  not null,
-    isLockedBy    int                                  null,
-    isLockedUntil datetime                             null,
-    status        enum ('draft','published','donated') not null,
-    occasion      varchar(255)                         null,
-    createdAt     timestamp DEFAULT CURRENT_TIMESTAMP,
-    updatedAt     timestamp ON UPDATE CURRENT_TIMESTAMP,
-    deletedAt     datetime                             null,
-    FOREIGN KEY (agencyId) REFERENCES agency (id),
-    FOREIGN KEY (createdBy) REFERENCES user (id),
-    FOREIGN KEY (isLockedBy) REFERENCES user (id),
-    FOREIGN KEY (childId) REFERENCES child (id)
+CREATE TYPE "ANIMAL_TYPE" AS ENUM (
+  'DOG',
+  'CAT',
+  'OTHER'
 );
 
-CREATE TABLE IF NOT EXISTS message
-(
-    id         int PRIMARY KEY AUTO_INCREMENT,
-    userId     int          not null,
-    wishcardId int          not null,
-    message    varchar(255) not null,
-    createdAt  timestamp DEFAULT CURRENT_TIMESTAMP,
-    updatedAt  timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deletedAt  datetime DEFAULT null,
-    FOREIGN KEY (userId) REFERENCES user (id),
-    FOREIGN KEY (wishcardId) REFERENCES wish_card (id)
+CREATE TABLE "user" (
+  "id" SERIAL PRIMARY KEY,
+  "firstName" varchar,
+  "lastName" varchar,
+  "email" varchar UNIQUE NOT NULL,
+  "uid" varchar UNIQUE NOT NULL,
+  "role" ROLES NOT NULL DEFAULT 'donor',
+  "createdAt" timestamp,
+  "updatedAt" timestamp,
+  "deletedAt" timestamp
 );
 
-CREATE TABLE IF NOT EXISTS donation
-(
-    id         int PRIMARY KEY AUTO_INCREMENT,
-    wishcardId int                                        not null,
-    userId     int                                        not null,
-    amount     float                                      not null,
-    status     enum ('confirmed', 'ordered', 'delivered') not null,
-    createdAt  timestamp DEFAULT CURRENT_TIMESTAMP,
-    updatedAt  timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deletedAt  datetime                                   null,
-    FOREIGN KEY (userId) REFERENCES user (id),
-    FOREIGN KEY (wishcardId) REFERENCES wish_card (id)
+CREATE TABLE "agencyMember" (
+  "userId" int NOT NULL,
+  "agencyId" int NOT NULL,
+  "agencyRole" AGENCY_ROLES DEFAULT 'member',
+  PRIMARY KEY ("userId", "agencyId")
 );
 
-ALTER TABLE agency
-    ADD FOREIGN KEY (addressId) REFERENCES address (id);
+CREATE TABLE "agency" (
+  "id" int PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "bio" varchar,
+  "isVerified" boolean DEFAULT false,
+  "phone" varchar,
+  "website" varchar,
+  "createdBy" int NOT NULL,
+  "addressId" int NOT NULL,
+  "createdAt" timestamp,
+  "updatedAt" timestamp,
+  "deletedAt" timestamp
+);
+
+CREATE TABLE "wishcard" (
+  "id" int PRIMARY KEY,
+  "imagePath" varchar NOT NULL,
+  "itemPrice" float NOT NULL,
+  "itemUrl" varchar NOT NULL,
+  "entityId" int NOT NULL,
+  "agencyId" int NOT NULL,
+  "createdBy" int NOT NULL,
+  "addressId" int NOT NULL,
+  "isLockedBy" int,
+  "isLockedUntil" timestamp,
+  "status" WISHCARD_STATUS NOT NULL DEFAULT 'draft',
+  "createdAt" timestamp,
+  "updatedAt" timestamp,
+  "deletedAt" timestamp
+);
+
+CREATE TABLE "child" (
+  "id" int PRIMARY KEY,
+  "birthday" date,
+  "gender" GENDER,
+  "firstName" varchar NOT NULL,
+  "lastName" varchar,
+  "interest" varchar,
+  "bio" varchar,
+  "createdAt" timestamp,
+  "updatedAt" timestamp,
+  "deletedAt" timestamp
+);
+
+CREATE TABLE "animal" (
+  "id" int PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "gender" GENDER NOT NULL DEFAULT 'other',
+  "type" ANIMAL_TYPE,
+  "breed" varchar,
+  "age" int,
+  "bio" varchar,
+  "createdAt" timestamp,
+  "updatedAt" timestamp,
+  "deletedAt" timestamp
+);
+
+CREATE TABLE "images" (
+  "id" int PRIMARY KEY,
+  "wishcardId" int NOT NULL,
+  "path" varchar NOT NULL
+);
+
+CREATE TABLE "address" (
+  "id" int PRIMARY KEY,
+  "address1" varchar NOT NULL,
+  "address2" varchar,
+  "city" varchar NOT NULL,
+  "country" varchar NOT NULL,
+  "state" varchar NOT NULL,
+  "createdAt" timestamp,
+  "updatedAt" timestamp,
+  "deletedAt" timestamp
+);
+
+CREATE TABLE "message" (
+  "id" int PRIMARY KEY,
+  "userId" int NOT NULL,
+  "wishcardId" int NOT NULL,
+  "message" varchar NOT NULL,
+  "createdAt" timestamp,
+  "updatedAt" timestamp,
+  "deletedAt" timestamp
+);
+
+CREATE TABLE "donation" (
+  "id" int PRIMARY KEY,
+  "wishcardId" int NOT NULL,
+  "userId" int NOT NULL,
+  "donationPrice" float NOT NULL,
+  "status" DONATION_STATUS NOT NULL DEFAULT 'donated',
+  "createdAt" timestamp,
+  "updatedAt" timestamp,
+  "deletedAt" timestamp
+);
+
+ALTER TABLE "agencyMember" ADD FOREIGN KEY ("userId") REFERENCES "user" ("id");
+
+ALTER TABLE "agencyMember" ADD FOREIGN KEY ("agencyId") REFERENCES "agency" ("id");
+
+ALTER TABLE "agency" ADD FOREIGN KEY ("createdBy") REFERENCES "user" ("id");
+
+ALTER TABLE "agency" ADD FOREIGN KEY ("addressId") REFERENCES "address" ("id");
+
+ALTER TABLE "wishcard" ADD FOREIGN KEY ("entityId") REFERENCES "child" ("id");
+
+ALTER TABLE "wishcard" ADD FOREIGN KEY ("entityId") REFERENCES "animal" ("id");
+
+ALTER TABLE "wishcard" ADD FOREIGN KEY ("agencyId") REFERENCES "agency" ("id");
+
+ALTER TABLE "wishcard" ADD FOREIGN KEY ("createdBy") REFERENCES "user" ("id");
+
+ALTER TABLE "wishcard" ADD FOREIGN KEY ("addressId") REFERENCES "address" ("id");
+
+ALTER TABLE "wishcard" ADD FOREIGN KEY ("isLockedBy") REFERENCES "user" ("id");
+
+ALTER TABLE "images" ADD FOREIGN KEY ("wishcardId") REFERENCES "wishcard" ("id");
+
+ALTER TABLE "message" ADD FOREIGN KEY ("userId") REFERENCES "user" ("id");
+
+ALTER TABLE "message" ADD FOREIGN KEY ("wishcardId") REFERENCES "wishcard" ("id");
+
+ALTER TABLE "donation" ADD FOREIGN KEY ("wishcardId") REFERENCES "wishcard" ("id");
+
+ALTER TABLE "donation" ADD FOREIGN KEY ("userId") REFERENCES "user" ("id");
+
+CREATE INDEX ON "user" ("uid");
+
+CREATE INDEX ON "user" ("email");
+
+CREATE INDEX ON "wishcard" ("agencyId");
+
+CREATE INDEX ON "wishcard" ("createdBy");
+
+CREATE INDEX ON "wishcard" ("status");
+
+CREATE INDEX ON "images" ("wishcardId");
+
+CREATE INDEX ON "message" ("wishcardId");
+
+CREATE INDEX ON "message" ("userId");
+
+CREATE INDEX ON "donation" ("wishcardId");
+
+CREATE INDEX ON "donation" ("userId");
