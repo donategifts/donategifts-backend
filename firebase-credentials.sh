1787e8d3ff6f2dec77938d8492a6ef5e6737b008
Enubia <marcoschuster@mailbox.org>
2021-08-10 12:02:16 +0200

    one more try

--- firebase-credentials.sh
+++ firebase-credentials.sh
@@ -1,27 +1,25 @@
-#!/usr/bin/env node
+#!/bin/bash
 
-(() => {
-  const fs = require('fs');
-  const path = require('path');
+TYPE="${{ secrets.FIREBASE_TOKEN_TYPE }}"
+PROJECT_ID="${{ secrets.FIREBASE_PROJECT_ID }}"
+PRIVATE_KEY_ID="${{ secrets.FIREBASE_PRIVATE_KEY_ID }}"
+PRIVATE_KEY="${{ secrets.FIREBASE_PRIVATE_KEY }}"
+CLIENT_EMAIL="${{ secrets.FIREBASE_CLIENT_EMAIL }}"
+CLIENT_ID="${{ secrets.FIREBASE_CLIENT_ID }}"
+AUTH_URI="${{ secrets.FIREBASE_AUTH_URI }}"
+TOKEN_URI="${{ secrets.FIREBASE_TOKEN_URI }}"
+AUTH_PROVIDER_X509_CERT_URL="${{ secrets.FIREBASE_AUTH_PROVIDER_X509_CERT_URL }}"
+CLIENT_X509_CERT_URL="${{ secrets.FIREBASE_CLIENT_X509_CERT_URL }"
 
-  try {
-    const fileContent = {
-      type: '${{ secrets.FIREBASE_TOKEN_TYPE }}',
-      project_id: '${{ secrets.FIREBASE_PROJECT_ID }}',
-      private_key_id: '${{ secrets.FIREBASE_PRIVATE_KEY_ID }}',
-      private_key: '${{ secrets.FIREBASE_PRIVATE_KEY }}',
-      client_email: '${{ secrets.FIREBASE_CLIENT_EMAIL }}',
-      client_id: '${{ secrets.FIREBASE_CLIENT_ID }}',
-      auth_uri: '${{ secrets.FIREBASE_AUTH_URI }}',
-      token_uri: '${{ secrets.FIREBASE_TOKEN_URI }}',
-      auth_provider_x509_cert_url:
-        '${{ secrets.FIREBASE_AUTH_PROVIDER_X509_CERT_URL }}',
-      client_x509_cert_url: '${{ secrets.FIREBASE_CLIENT_X509_CERT_URL }}',
-    };
-
-    fs.writeFileSync('donate-gifts.json', JSON.stringify(fileContent, null, 2));
-  } catch (e) {
-    console.error(e);
-    process.exit(0);
-  }
-})();
+echo "{
+  "type": $TYPE,
+  "project_id": $PROJECT_ID,
+  "private_key_id": $PRIVATE_KEY_ID,
+  "private_key": $PRIVATE_KEY,
+  "client_email": $CLIENT_EMAIL,
+  "client_id": $CLIENT_ID,
+  "auth_uri": $AUTH_URI,
+  "token_uri": $TOKEN_URI,
+  "auth_provider_x509_cert_url": $AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": $CLIENT_X509_CERT_URL
+}" > donate-gifts.json
